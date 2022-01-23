30de6aaeb9f197abeab3e9734eeb594edf231a53
Darshan <aravinddarshan13897@outlook.com>
2022-01-23 12:15:38 +0530

    Time: 2219 ms (25.00%), Space: 37.6 MB (75.00%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.py
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.py
@@ -1,12 +1,11 @@
+
 class Solution:
     def findLonely(self, nums: List[int]) -> List[int]:
         cnt = Counter(nums)
-        s = set(nums)
         res = []
         for e in nums:
-            if cnt[e] > 1 or e + 1 in s or e - 1 in s:
+            if cnt[e] > 1 or e + 1 in cnt or e - 1 in cnt:
                 continue
             else:
                 res.append(e)
-
         return res
