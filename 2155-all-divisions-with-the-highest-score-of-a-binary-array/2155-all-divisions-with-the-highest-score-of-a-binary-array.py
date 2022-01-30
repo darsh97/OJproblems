c35bebbabb8656f284db53e665502e41ecb7d2e7
Darshan <aravinddarshan13897@outlook.com>
2022-01-30 12:09:37 +0530

    Time: 3808 ms (83.33%), Space: 25.3 MB (33.33%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -1,23 +1,15 @@
 class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        zero_prefix = one_suffix = 0
-        zero_prefix_array, one_suffix_array = [], []
-        res = []
+        zero = 0
+        one = sum(nums)
+        res = [one, ]
+        mx = res[0]
 
         for i in range(n):
-            zero_prefix += not nums[i]
-            one_suffix += nums[n - i - 1]
-
-            zero_prefix_array.append(zero_prefix);
-            one_suffix_array.append(one_suffix)
-
-        one_suffix_array.reverse()
-
-        for i in range(n - 1):
-            res.append(zero_prefix_array[i] + one_suffix_array[i + 1])
-
-        res = [one_suffix_array[0]] + res + [zero_prefix_array[-1]]
-        mx = max(res)
+            zero += not nums[i]
+            one -= nums[i]
+            res.append(zero + one)
+            mx = max(mx, res[-1])
 
         return [i for i, e in enumerate(res) if e == mx]
