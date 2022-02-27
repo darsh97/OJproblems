bce77371d340a52a3f8aaf69952a6ed9559b110c
Darshan <aravinddarshan13897@outlook.com>
2022-02-27 10:52:32 +0530

    Time: 3761 ms (70.00%), Space: 28.8 MB (10.00%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -2,8 +2,8 @@ class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
         lb = 0
         rb = max(time) * totalTrips
-
         min_time = None
+        
         def get_trips(k) -> bool:
             return sum(k // t for t in time) >= totalTrips
 
