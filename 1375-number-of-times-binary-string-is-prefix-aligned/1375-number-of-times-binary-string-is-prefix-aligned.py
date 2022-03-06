9aef5defa4b167fbd69505efa2d98a2413ae62e4
Darshan <aravinddarshan13897@outlook.com>
2022-03-06 18:09:43 +0530

    Time: 535 ms (57.85%), Space: 21.2 MB (40.23%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
@@ -1,12 +1,11 @@
 class Solution:
     def numTimesAllBlue(self, light: List[int]) -> int:
         last_one = -inf
-        zero_heap = [*range(1, len(light) + 1)]
-        heapq.heapify(zero_heap)
+        first_zero = 1
         cnt = 0
         for l in light:
-            first_zero = heapq.heappop(zero_heap)
             last_one = max(last_one, l)
             if first_zero >= last_one:
                 cnt += 1
+            first_zero += 1
         return cnt
