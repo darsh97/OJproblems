dac5639f44359d864317aee11438b4839c73c2a2
Darshan <aravinddarshan13897@outlook.com>
2022-04-10 17:08:29 +0530

    Time: 1290 ms (25.86%), Space: 24.1 MB (37.10%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,63 @@
+class MinMaxQueue(deque):
+    def __init__(self):
+        super().__init__()
+        self.Q = deque([])
+        self._miQ = deque([])
+        self._mxQ = deque([])
+
+    def enque(self, value):
+        if not self.Q:
+            self.Q.append(value)
+            self._miQ.append(value)
+            self._mxQ.append(value)
+        else:
+            self.Q.append(value)
+
+            while self._miQ and self._miQ[-1] > value:
+                self._miQ.pop()
+
+            self._miQ.append(value)
+
+            while self._mxQ and self._mxQ[-1] < value:
+                self._mxQ.pop()
+
+            self._mxQ.append(value)
+
+    def deque(self):
+        if self.Q[0] == self._miQ[0]:
+            self.Q.popleft()
+            self._miQ.popleft()
+
+        elif self.Q[0] == self._mxQ[0]:
+            self.Q.popleft()
+            self._mxQ.popleft()
+
+        else:
+            self.Q.popleft()
+
+    @property
+    def get_max(self):
+        return self._mxQ[0]
+
+    @property
+    def get_min(self):
+        return self._miQ[0]
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        nums = nums + [0]
+
+        q = MinMaxQueue()
+        res = 1
+
+        for e in nums:
+            if not q.Q:
+                q.enque(e)
+            else:
+                while q.Q and q.get_max - q.get_min > limit:
+                    q.deque()
+                res = max(len(q.Q), res)
+                q.enque(e)
+
+        return res
